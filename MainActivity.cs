a7df4b92b634fe2cc4c2f14fda98dec5888009e3
liang.good <liang.good@hotmail.com>
2026-09-09 08:42:58 +0800

    添加项目文件。

---
+++ MainActivity.cs
@@ -0,0 +1,20 @@
+namespace XMedicalAndroid;
+
+[Activity(Label = "@string/app_name", MainLauncher = true)]
+public class MainActivity : Activity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // Set our view from the "main" layout resource
+        SetContentView(Resource.Layout.activity_main);
+
+        Init();
+    }
+
+    void Init()
+    {
+        ActionBar?.Hide();
+    }
+}
